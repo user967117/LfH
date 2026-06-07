e318362ef09ccf0869c03bf11bab3fae66a5bc89
user967117 <sashunua.dor@gmail.com>
2026-06-07 16:01:53 +0300

    add registration

--- LawFirmsHelper/LawFirmsHelper/Program.cs
+++ LawFirmsHelper/LawFirmsHelper/Program.cs
@@ -1,15 +1,18 @@
 using Microsoft.EntityFrameworkCore;
 
 using LawFirmsHelper;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen();
-
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<IdentityUser>().AddEntityFrameworkStores<AppDbContext>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddOpenApi();
 
@@ -19,4 +22,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapIdentityApi<IdentityUser>();
+
 app.Run();
