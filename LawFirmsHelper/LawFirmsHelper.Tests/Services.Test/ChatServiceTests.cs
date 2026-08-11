526a21edf7ce046dc1e5491838e82a5c66b0e1ba
user967117 <sashunua.dor@gmail.com>
2026-08-11 16:57:37 +0300

    edit

--- LawFirmsHelper/LawFirmsHelper.Tests/Services.Test/ChatServiceTests.cs
+++ LawFirmsHelper/LawFirmsHelper.Tests/Services.Test/ChatServiceTests.cs
@@ -3,6 +3,7 @@ using LawFirmsHelper.Repositories;
 using LawFirmsHelper.Requests;
 using LawFirmsHelper.Services;
 using Moq;
+using NSubstitute;
 
 namespace TestProject1;
 
@@ -12,23 +13,50 @@ public class ChatServiceTests
     public async Task GetChatHistoryAsync_ShouldReturnChatHistory()
     {
         // arrange
-        var chatRepo = new Mock<IChatRepository>();
-        var leadRepo = new Mock<ILeadRepository>();
-        var messageRepo = new Mock<IMessageRepository>();
+        var chatRepo = Substitute.For<IChatRepository>();
+        var leadRepo = Substitute.For<ILeadRepository>();
+        var messageRepo = Substitute.For<IMessageRepository>();
         
-        var service = new ChatService(chatRepo.Object, leadRepo.Object, messageRepo.Object);
+        var service = new ChatService(chatRepo, leadRepo, messageRepo);
         
         var chatId = Guid.NewGuid();
         var request = new SearchRequest{Offset =  0, Limit = 50};
+
+        var fakeActor = new Actor
+        {
+            Id = Guid.NewGuid(),
+            Type = ActorType.Lead
+        };
+
+        var fakeMessage = new List<Message>
+        {
+            new Message
+            {
+                Id = Guid.NewGuid(),
+                ChatId = chatId,
+                Text = "Hello World!",
+                CreatedAt = DateTime.UtcNow,
+                Actor = fakeActor
+            }
+        };
         
-        messageRepo.Setup(repo => repo.GetMessageByChatIdAsync(chatId, request.Offset, request.Limit, It.IsAny<CancellationToken>()))
-            .ReturnsAsync(new List<Message>());
+        messageRepo.GetMessageByChatIdAsync(chatId, request.Offset, request.Limit, Arg.Any<CancellationToken>())
+            .Returns(fakeMessage);
         
         // act
         var result = await service.GetChatHistoryAsync(chatId, request);
         
         // assert
         Assert.NotNull(result);
-        Assert.Empty(result);
+        Assert.Single(result);
+        
+        var responseItem = result.First();
+        
+        Assert.Equal(fakeMessage[0].Id, responseItem.Id);
+        Assert.Equal(fakeMessage[0].Text, responseItem.Text);
+        Assert.Equal(fakeMessage[0].CreatedAt, responseItem.CreatedAt);
+        
+        Assert.Equal(fakeActor.Id, responseItem.ActorId);
+        Assert.Equal(fakeActor.Type, responseItem.ActorType);
     }
 }
