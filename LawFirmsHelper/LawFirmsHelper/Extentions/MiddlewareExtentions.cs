629fc1b94e22b73500296ee22b56ee8825d0a1e2
user967117 <sashunua.dor@gmail.com>
2026-09-09 12:34:08 +0300

    Update MiddlewareExtentions.cs

--- LawFirmsHelper/LawFirmsHelper/Extentions/MiddlewareExtentions.cs
+++ LawFirmsHelper/LawFirmsHelper/Extentions/MiddlewareExtentions.cs
@@ -8,13 +8,9 @@ public static class MiddlewareExtentions
     public static IApplicationBuilder UseMiddlewares(this WebApplication app)
     {
         
-        
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
+        app.UseSwagger();
+        app.UseSwaggerUI();
+            
         app.UseHttpsRedirection();
         
         app.UseAuthentication();
